f35aaaa2bb035ffaaf666eb157ddddd05ea1c187
repmacrypn <artik3267@gmail.com>
2023-07-21 21:22:19 +0300

    vite app setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "indent": [
+      "error",
+      4
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-console": "warn",
+    "prefer-const": "error",
+    "max-len": [
+      "error",
+      {
+        "code": 170
+      }
+    ],
+    "comma-dangle": [
+      "error",
+      "always-multiline"
+    ],
+    "no-unused-vars": "warn",
+    "linebreak-style": [
+      "error",
+      "unix"
+    ]
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable indent */
+
+const App = () => {
+  return (
+    <div>
+      qqhj
+    </div>
+  )
+}
+
+export default App
