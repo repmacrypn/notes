87468991cd1edf478ba7bb12622779ffafd405e4
repmacrypn <artik3267@gmail.com>
2023-07-24 03:38:40 +0300

    gh pages injected

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/notes/',
   plugins: [react()],
 })
